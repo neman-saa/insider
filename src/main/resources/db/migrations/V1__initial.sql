2fcd45c2bf722fe6cd554525ac62f1cb329b2a0d
Alexey Suslov <asuslov@evolution.com>
2025-12-15 20:20:21 +0200

    Updated schema

--- src/main/resources/db/migrations/V1__initial.sql
+++ src/main/resources/db/migrations/V1__initial.sql
@@ -1,18 +1,62 @@
-CREATE TABLE markets (
-    conditionId TEXT PRIMARY KEY,
-    id TEXT NOT NULL,
-    question TEXT NOT NULL,
-    volume DOUBLE PRECISION NOT NULL,
-    tokens TEXT[] NOT NULL CHECK (cardinality(tokens) >= 2)
+CREATE TABLE "markets"
+(
+    "id"                 varchar PRIMARY KEY,
+    "creator_address_id" varchar,
+    "question"           varchar,
+    "description"        varchar,
+    "start_date"         timestamp,
+    "end_date"           timestamp,
+    "created_at"         timestamp
 );
 
-CREATE TABLE trades (
-    conditionId TEXT REFERENCES markets(conditionId) ON DELETE CASCADE,
-    wallet TEXT NOT NULL,
-    outcome TEXT CHECK (outcome IN ('YES', 'NO', 'OTHER')) NOT NULL,
-    outcomeMessage TEXT,
-    tokenId TEXT NOT NULL,
-    totalSize DOUBLE PRECISION NOT NULL,
-    totalPrice DOUBLE PRECISION NOT NULL,
-    PRIMARY KEY (conditionId, tokenId)
+CREATE TABLE "events"
+(
+    "id"          varchar PRIMARY KEY,
+    "market_id"   varchar,
+    "ticker"      varchar,
+    "title"       varchar,
+    "description" varchar
 );
+
+CREATE TABLE "emissions"
+(
+    "id"                 uuid PRIMARY KEY,
+    "market_id"          uuid,
+    "emitent_address_id" varchar,
+    "amount"             integer,
+    "created_at"         timestamp
+);
+
+CREATE TABLE "trades"
+(
+    "id"               uuid PRIMARY KEY,
+    "saler_address_id" varchar,
+    "buyer_address_id" varchar,
+    "event_id"         varchar,
+    "amount"           integer,
+    "price"            decimal,
+    "created_at"       timestamp
+);
+
+CREATE TABLE "resolutions"
+(
+    "id"         uuid PRIMARY KEY,
+    "market_id"  varchar,
+    "true_event" varchar,
+    "created_at" timestamp
+);
+
+ALTER TABLE "emissions"
+    ADD FOREIGN KEY ("market_id") REFERENCES "markets" ("id");
+
+ALTER TABLE "events"
+    ADD FOREIGN KEY ("market_id") REFERENCES "markets" ("id");
+
+ALTER TABLE "trades"
+    ADD FOREIGN KEY ("event_id") REFERENCES "events" ("id");
+
+ALTER TABLE "resolutions"
+    ADD FOREIGN KEY ("market_id") REFERENCES "markets" ("id");
+
+ALTER TABLE "resolutions"
+    ADD FOREIGN KEY ("true_event") REFERENCES "events" ("id");
