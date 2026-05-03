9ee90ddbcaf2d7393bcc047c1634705fb7e7f432
neman <neman.saa@gmail.com>
2026-05-03 15:26:40 +0300

    changing insert token infos method

--- src/main/scala/org/github/insider/realtime/tokens/TokensInfoRepositoryImpl.scala
+++ src/main/scala/org/github/insider/realtime/tokens/TokensInfoRepositoryImpl.scala
@@ -15,17 +15,22 @@ import java.time.Instant
 class TokensInfoRepositoryImpl[F[_]: Async](transactor: Transactor[F], logger: Logger[F])
     extends TokensInfoRepository[F] {
 
-  override def insert(tokens: NonEmptyList[TokenInfo]): F[Unit] =
-    Update[TokenInfo](
-      """
-        |INSERT INTO tokens_info (id, price, score, resolve_date, last_updated_block_num)
-        |VALUES (?, ?, ?, ?, ?)
-        |""".stripMargin
-    )
-      .updateMany(tokens)
+  override def insert(tokens: NonEmptyList[TokenInfo]): F[Unit] = {
+
+    val initFragment = fr" INSERT INTO tokens_info (id, price, score, resolve_date, last_updated_block_num) VALUES"
+    val fragments =
+      tokens.map(token =>
+        fr"(${token.id}, ${token.price}, ${token.score}, ${token.resolveDate}, ${token.lastUpdatedBlock})"
+      )
+
+    val request = initFragment ++ fragments.reduce(_ ++ _)
+    request
+      .update
+      .run
       .transact(transactor)
       .flatTap(rows => logger.info(s"Inserted $rows tokens info in 'tokens_info' table"))
       .void
+  }
 
   override def getForTokens(tokens: List[String]): F[Map[String, TokenInfo]] =
     fr"""
