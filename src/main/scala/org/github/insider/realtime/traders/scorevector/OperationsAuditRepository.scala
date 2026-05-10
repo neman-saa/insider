175191495a89dc7584c3a67cf8687890683175fb
gosterx <aliaksei.suslau02@gmail.com>
2026-05-10 23:17:34 +0200

    Fix ZonedDateTime to Instant

--- src/main/scala/org/github/insider/realtime/traders/scorevector/OperationsAuditRepository.scala
+++ src/main/scala/org/github/insider/realtime/traders/scorevector/OperationsAuditRepository.scala
@@ -10,7 +10,7 @@ import org.github.insider.polymarket.domain.Side
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 
-import java.time.Instant
+import java.time.{Instant, ZonedDateTime}
 
 trait OperationsAuditRepository[F[_]] {
   def insert(auditLog: OperationAuditLog): F[Unit]
@@ -39,12 +39,12 @@ object OperationsAuditRepository {
 
     override def selectAll: F[List[OperationAuditLog]] =
       selectAllQuery
-        .query[(String, Side, Option[BigDecimal], BigDecimal, Instant)]
+        .query[(String, Side, Option[BigDecimal], BigDecimal, ZonedDateTime)]
         .map[OperationAuditLog] {
           case (tokenId, Side.Buy, momentScore, price, timestamp) =>
-            OperationAuditLog.BuyAuditLog(tokenId, momentScore, price, timestamp)
+            OperationAuditLog.BuyAuditLog(tokenId, momentScore, price, timestamp.toInstant)
           case (tokenId, Side.Sell, momentScore, price, timestamp) =>
-            OperationAuditLog.SellAuditLog(tokenId, momentScore, price, timestamp)
+            OperationAuditLog.SellAuditLog(tokenId, momentScore, price, timestamp.toInstant)
         }
         .to[List]
         .transact(transactor)
